6ac867257e56499d841161268ee058b254cdb22a
coding_bot <68002888+DataSC3@users.noreply.github.com>
2020-07-17 11:25:29 +0300

    Add files via upload

--- No-BlackMail.py
+++ No-BlackMail.py
@@ -14,7 +14,6 @@ URL_L = '\033[36m'
 LI_G='\033[92m'
 F_CL = '\033[0m'
 
-
 if os.path.exists('.banner_840'):
     pass
 else:
@@ -25,35 +24,42 @@ else:
         print(f'{LI_G}Этот текст покажется лишь один раз!{RESET}')
         open('.banner_840','w')
         input(f'{LI_G}Напишите что нибудь чтобы очистить экран: {RESET}')
+        os.system('clear')
     except FileNotFoundError:
         pass
-    os.system('clear')
-
+    
 if os.path.exists('dataFile.txt'):
     try:
         os.system('clear')
         print(f'{CYAN}{BOLD}[1] {LI_G}Перезаписать данные в файл.{RESET}')
-        print(f'{CYAN}{BOLD}[2] {LI_G}Добавить к остальным.{RESET}\n')
+        print(f'{CYAN}{BOLD}[ENTER] {LI_G}Добавить к остальным.{RESET}\n')
         dataV = input(f'{CYAN}{BOLD}[~] {LI_G}Выберите метод: {RESET}')
         if dataV == '1':
             os.remove('dataFile.txt')
             os.system('clear') 
-            print(f'{YELLOW}{BOLD}[+] {LI_G}Данные будут:{RESET} Перезаписаны\n')
+            print(f'{YELLOW}{BOLD}[+] {LI_G}Данные будут:{RESET} Перезаписаны')
+            sleep(1)
         elif dataV == '2':
             os.system('clear')
-            print(f'{YELLOW}{BOLD}[+] {LI_G}Данные будут:{RESET} Добавляться\n')
+            print(f'{YELLOW}{BOLD}[+] {LI_G}Данные будут:{RESET} Добавлены к остальным')
+            sleep(1)
         else:
-            sys.exit(f'{YELLOW}{BOLD}[+] {RED}Выберите метод работы пожалуйста{RESET}')
+            os.system('clear')
+            print(f'{YELLOW}{BOLD}[+] {LI_G}Данные будут:{RESET} Добавлены к остальным')
+            sleep(1)
     except KeyboardInterrupt:
         sys.exit(f'\n{CYAN}{BOLD}[!] {RED}Принудительная остановка кода{RESET}')
 else:
-   pass
+    os.system('clear')
+print(f'{YELLOW}{BOLD}[?] {LI_G}Поиск данных о номерах всех стран. {RESET}')
+sleep(2)
 os.system('clear')
+print(f'{YELLOW}{BOLD}[#] {LI_G}Подготовка... {RESET}')
 def getNumber():
     try:
         while type:
             try:
-                versioUR = requests.get('https://github.com/DataSC3/No-BlackM/commits/master')
+                versioUR = requests.get('https://github.com/Eric-M1/File-C/commits/master')
                 versioURL = bs(versioUR.text, 'html.parser')
                 get_version = versioURL.find_all('div', class_='commit-desc')
                 with open('.banner_840', 'r') as fileF:
@@ -70,6 +76,7 @@ def getNumber():
                             fileW.write('Version:'+str(len(get_version)))
             except:
                 pass
+            os.system('clear')
             print(f'{YELLOW}{BOLD}[~] {LI_G}Пример: {RESET}7ХХХХХХХХХ')
             getTempNumber=input(f'{YELLOW}{BOLD}[~] {LI_G}Введите номер: {RESET}+')
             try:                                      
@@ -87,7 +94,7 @@ try:
     num_P_S = requests.post('https://htmlweb.ru/geo/api.php?json&telcod='+str(number))
     num_P = num_P_S.json()
     
-    print(f'{YELLOW}{BOLD}[+] {LI_G}Поиск данных... {RESET}\n')
+    print(f'{YELLOW}{BOLD}[~] {LI_G}Поиск данных... {RESET}\n')
     try:
         country = num_P['country']
         for a in country:
@@ -160,8 +167,9 @@ try:
                 else:
                     count = 0
                     h1T = h1.text.replace("  ","")
-                    print(f'\n{YELLOW}{BOLD}[+] {LI_G}Поиск данных по Авито: {RESET}')
-                    print(f'{YELLOW}{BOLD}[+] {LI_G}Найден: {F_CL}{h1T}\n{RESET}')
+                    print(f'\n{YELLOW}{BOLD}[~] {LI_G}Поиск данных по Авито: {RESET}')
+                    print(f'{YELLOW}{BOLD}[~] {LI_G}Авито: {F_CL}{h1T}{RESET}')
+                    print(f'{YELLOW}{BOLD}[+] {LI_G}----------------------------------------- >{RESET}\n')
                     for oBV in contentAV.find_all(['h4', 'span']):
                         print(f'{YELLOW}{BOLD}[+] {LI_G}{oBV.text}{RESET}')  
                         dataOB.append(oBV.text)
@@ -169,36 +177,39 @@ try:
                     with open('dataFile.txt', 'a', encoding='utf-8') as f:
                         for data in dataOB:
                             f.write('[-] '+ data +'\n')
-
+                    
                     for url in contentAV.find_all(['a']):
                         count += 1
                         user_link = url['href']
-                        user_url = requests.get('https://mirror.bullshit.agency'+user_link)
+                        try:            
+                            avito_url = requests.get('https://mirror.bullshit.agency'+user_link)
+                            content = bs(avito_url.text, 'html.parser')
+                            url = content.find(['a'])
+                            
+                            linkAV = url['href']
+                            print(f'{YELLOW}{BOLD}[{count}] {URL_L}{UNDERLINE}{linkAV}{RESET}')
+                            
+                            u_name = bs(avito_url.text, 'html.parser')
+                            nameU = u_name.find('strong')
+
+                            name.append(nameU.text)
+                            dataAV.append(f'[{count}] {linkAV}')
+
+                        except:
+                            print(f'{YELLOW}{BOLD}[{count}] {RED}{UNDERLINE}{user_link}{RESET}')
+                            continue
                         
-                        avito_url = requests.get('https://mirror.bullshit.agency'+user_link)
-                        content = bs(avito_url.text, 'html.parser')
-                        url = content.find(['a'])
-                        
-                        linkAV = url['href']
-                        print(f'{YELLOW}{BOLD}[{count}] {URL_L}{UNDERLINE}{linkAV}{RESET}')
-
-                        u_name = bs(user_url.text, 'html.parser')
-                        nameU = u_name.find('strong')
-
-                        name.append(nameU.text)
-                        dataAV.append(f'[{count}] {linkAV}')
-
                     with open('dataFile.txt', 'a', encoding='utf-8') as fileD:
                         fileD.write('[∩] Номер: +'+str(number)+'\n')
                         for data in dataAV:
                             fileD.write(data +'\n')
                         fileD.write(f'[-] https://api.whatsapp.com/send?phone={str(number)}&text=Hello,%20this%20is%20NO-Blackmail')
-                        fileD.write(f'\n[-] Все имена с ссылок: {str(name)}\n\n')
+                        fileD.write('\n[-] Все имена с ссылок: ' + ', '.join(name) +'\n\n')
                     if not name:
                         pass
                     else:
-                        print(f'\n{YELLOW}{BOLD}[+] {LI_G}Все имена с ссылок: {RESET}{str(name)}')
-                    print(f'{YELLOW}{BOLD}[+] {LI_G}Данные о номере: +{str(number)} добавлены в файл {RESET}dataFile.txt')
+                        print('\n'+YELLOW+BOLD+'[~]'+LI_G+' Все имена с ссылок: '+RESET+', '.join(name))
+                    print(f'{YELLOW}{BOLD}[~] {LI_G}Данные о номере: +{str(number)} добавлены в файл {RESET}dataFile.txt')
             
             get_url_name_avito(number)
             
@@ -211,11 +222,11 @@ try:
                 for data in dataAV:
                     fileD.write(data +'\n')
                 fileD.write(f'[-] https://api.whatsapp.com/send?phone={str(number)}&text=Hello,%20this%20is%20NO-Blackmail')
-                fileD.write(f'\n[-] Все имена с ссылок: {str(name)}\n\n')
-            print(f'{YELLOW}{BOLD}[!] {RED}Ваш запрос временно заблокирован или Ссылки/Имена не найдены. Пожалуйста, подождите 4-10 минуты.{RESET}')
+                fileD.write('\n[-] Все имена с ссылок: ' + ', '.join(name) +'\n\n')
+            print(f'{YELLOW}{BOLD}[!] {RED}Ваш запрос временно заблокирован. Пожалуйста, подождите 2-6 минуты.{RESET}')
                 
-        print(f'{YELLOW}{BOLD}[+] {LI_G}Создан прямая ссылка в WhatsApp: {RESET}')
-        print(f'{YELLOW}{BOLD}[+] {URL_L}{UNDERLINE}https://api.whatsapp.com/send?phone={str(number)}&text=Hello,%20this%20is%20No-BlackMail{RESET}')
+        print(f'{YELLOW}{BOLD}[~] {LI_G}Создан прямая ссылка в WhatsApp: {RESET}')
+        print(f'{YELLOW}{BOLD}[~] {URL_L}{UNDERLINE}https://api.whatsapp.com/send?phone={str(number)}&text=Hello,%20this%20is%20No-BlackMail{RESET}')
     print(f'\n{YELLOW}{BOLD}[!] {RED}Всего лимитов: {str(num_P["limit"])}{RESET}')
 
 except KeyboardInterrupt:
